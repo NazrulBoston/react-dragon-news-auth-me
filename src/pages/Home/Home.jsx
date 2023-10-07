226f72afdc0da30eb10fdd7ad1e80d15727a2728
Nazrul Islam <dobon.islam@icloud.com>
2023-10-06 20:01:48 -0400

    card details added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,30 @@
+import { useLoaderData } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 import BreakingNews from "./BreakingNews";
+import NewsCard from "./NewsCard";
 
 
 const Home = () => {
+    const news = useLoaderData();
+    console.log(news)
     return (
         <div>
             <Header></Header>
             <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
-                <div className="border-2">
+                <div className="">
                     <LeftSideNav></LeftSideNav>
                 </div>
-                <div className="lg:col-span-2 border-2" >
-                    <h2 className="text-2xl">News coming soon..</h2>
+                <div className="lg:col-span-2" >
+                    {
+                        news.map(aNews => <NewsCard key={aNews._id} news= {aNews}></NewsCard>)
+                    }
                 </div>
-                <div className="border-2">
+                <div>
                     <RightSideNav></RightSideNav>
                 </div>
             </div>
